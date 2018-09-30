60bd7c838f6087c9d100b6355135031180a0d2a5
liuhao <1074345116@qq.com>
2018-09-30 22:19:06 +0800

    ...

--- src/main/java/com/love_peace/blogmgm/BlogMgmApplication.java
+++ src/main/java/com/love_peace/blogmgm/BlogMgmApplication.java
@@ -7,10 +7,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 @EnableJpaAuditing
 public class BlogMgmApplication {
-
-
-
-
     public static void main(String[] args) {
         SpringApplication.run(BlogMgmApplication.class, args);
     }
