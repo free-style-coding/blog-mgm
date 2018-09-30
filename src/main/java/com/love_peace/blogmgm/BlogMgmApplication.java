232ba685fb40cb974723e7afe6616a87b395b5ba
zenghe <13164307787@163.com>
2018-09-30 22:42:18 +0800

    ....

--- src/main/java/com/love_peace/blogmgm/BlogMgmApplication.java
+++ src/main/java/com/love_peace/blogmgm/BlogMgmApplication.java
@@ -7,6 +7,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 @EnableJpaAuditing
 public class BlogMgmApplication {
+
+
     public static void main(String[] args) {
         SpringApplication.run(BlogMgmApplication.class, args);
     }
